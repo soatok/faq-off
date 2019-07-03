cef8b558100f475fe29c2c2305a2e6783222dc3b
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-03 01:57:26 -0400

    Begin Collections front page

--- src/FaqOff/Endpoints/EntryCollection.php
+++ src/FaqOff/Endpoints/EntryCollection.php
@@ -7,7 +7,10 @@ use Psr\Http\Message\{
     RequestInterface,
     ResponseInterface
 };
+use Slim\Container;
 use Soatok\AnthroKit\Endpoint;
+use Soatok\FaqOff\MessageOnceTrait;
+use Soatok\FaqOff\Splices\Authors;
 use Twig\Error\{
     LoaderError,
     RuntimeError,
@@ -20,17 +23,61 @@ use Twig\Error\{
  */
 class EntryCollection extends Endpoint
 {
+    use MessageOnceTrait;
+
+    /** @var Authors $authors */
+    private $authors;
+
+    /** @var \Soatok\FaqOff\Splices\EntryCollection $collections */
+    private $collections;
+
+    public function __construct(Container $container)
+    {
+        parent::__construct($container);
+        $this->authors = $this->splice('Authors');
+        $this->collections = $this->splice('EntryCollection');
+    }
+
     /**
      * @param RequestInterface $request
      * @param ResponseInterface|null $response
      * @param array $routerParams
      * @return ResponseInterface
+     *
+     * @throws ContainerException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function __invoke(
         RequestInterface $request,
         ?ResponseInterface $response = null,
         array $routerParams = []
     ): ResponseInterface {
-        return $this->json($routerParams);
+        if (empty($routerParams['author'])) {
+            return $this->redirect('/');
+        }
+        if (empty($routerParams['collection'])) {
+            return $this->redirect('/');
+        }
+        $author = $this->authors->getByScreenName($routerParams['author']);
+        if (!$author) {
+            $this->messageOnce('Author not found.', 'error');
+            return $this->redirect('/');
+        }
+
+        $authorId = (int) $author['authorid'];
+        $collection = $this->collections->getByAuthorAndUrl(
+            $authorId,
+            $routerParams['collection']
+        );
+        if (!$collection) {
+            $this->messageOnce('Collection does not exist.', 'error');
+            return $this->redirect('/@' . $author['screenname']);
+        }
+
+        return $this->view('collection.twig', [
+            'collection' => $collection
+        ]);
     }
 }
