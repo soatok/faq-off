27786dcc9f650b3d466741227afabef542c6e6f2
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-02 22:19:28 -0400

    Use PostgreSQL 9.6

--- tests/autoload-phpunit.php
+++ tests/autoload-phpunit.php
@@ -30,8 +30,6 @@ if (is_readable(APP_ROOT . '/local/phpunit.php')) {
 }
 $app = new \Slim\App($settings);
 
-var_dump($settings);
-
 // Set up dependencies
 require APP_ROOT . '/src/dependencies.php';
 
